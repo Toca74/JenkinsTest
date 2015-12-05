6f9465e2603674245927c6c4b74d5195f0fca289
Toca74 <toca74@users.noreply.github.com>
2015-12-05 14:51:56 +0000

    Update test description

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with correct text', function(done) {
+  it('respond with the correct text', function(done) {
     request(app).get('/').expect('jenkins build and deploy', done);
   });
 });
